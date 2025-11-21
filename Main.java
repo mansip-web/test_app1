ba2119f79253b018d3e1ce073f213aa95ec2d2d7
Mansi <mansip@heaptrace.com>
2025-11-21 11:18:24 +0530

    initial commit4

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello! This is line 1.");
+        System.out.println("This is line 2.");
+        System.out.println("This is line 3.");
+    }
+}
